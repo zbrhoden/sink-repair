e768940aded46582f9293188fb440b10c6a1a869
Blake Rhoden <zbrhoden@gmail.com>
2021-08-12 14:04:13 -0500

    requests

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -1,4 +1,16 @@
 const applicationState = {
+    requests = []
+}
+const API = "http://localhost:8088"
 
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
 }
 
