8fd5dceedc23eff642c1aa8226a9041bfe6c86cc
Blake Rhoden <zbrhoden@gmail.com>
2021-08-13 10:37:32 -0500

    requests

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,12 +1,20 @@
 import { getRequests } from "./dataAccess.js"
 
+
+const convertRequestToListElement = () => {
+    return `<li>
+    Request #${requests.id}
+    </li>`
+}
+
+
 export const Requests = () => {
     const requests = getRequests()
 
     let html = `
         <ul>
             ${
-                requests.map()
+                requests.map(convertRequestToListElement())
             }
         </ul>
     `
