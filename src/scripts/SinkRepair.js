b175b0389fc3e60e97b2bdd80433aaec2d21a207
Blake Rhoden <zbrhoden@gmail.com>
2021-08-13 10:17:30 -0500

    interpolation

--- src/scripts/SinkRepair.js
+++ src/scripts/SinkRepair.js
@@ -1,10 +1,11 @@
+import { ServiceForm } from "./ServiceForm.js"
 import { Requests } from "./Requests.js"
-
 
 export const SinkRepair = () => {
     return `
         <h1>Maude and Merle's Sink Repair</h1>
         <section class="serviceForm">
+            ${ServiceForm()}
         </section>
 
         <section class="serviceRequests">
